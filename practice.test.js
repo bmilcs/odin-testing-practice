d10d84ad430a758bac582f6bab298a04bb16f1ce
Bryan Miller <bmilcs@yahoo.com>
2022-11-23 14:50:51 -0500

    Add calculator tests & object w/ methods

--- practice.test.js
+++ practice.test.js
@@ -1,5 +1,6 @@
 import capitalize from "./capitalize";
 import reverseString from "./reverseString";
+import calculator from "./calculator";
 
 test("capitalize: basketball", () => {
   expect(capitalize("basketball")).toStrictEqual("Basketball");
@@ -15,4 +16,20 @@ test("reverse string: shark", () => {
 
 test("reverse string: my name is", () => {
   expect(reverseString("my name is")).toStrictEqual("si eman ym");
+});
+
+test("calculator: add 5+5", () => {
+  expect(calculator.add(5, 5)).toStrictEqual(10);
+});
+
+test("calculator: subtract 10-5", () => {
+  expect(calculator.subtract(10, 5)).toStrictEqual(5);
+});
+
+test("calculator: multiply 10, 5", () => {
+  expect(calculator.multiply(10, 5)).toStrictEqual(50);
+});
+
+test("calculator: divide 100, 5", () => {
+  expect(calculator.divide(100, 5)).toStrictEqual(20);
 });
