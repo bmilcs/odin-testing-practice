9228e5a8ea743ec6361fe07451a674284ebc07bb
Bryan Miller <bmilcs@yahoo.com>
2022-11-23 14:42:34 -0500

    Add reverseString tests & module

--- practice.test.js
+++ practice.test.js
@@ -1,9 +1,18 @@
 import capitalize from "./capitalize";
+import reverseString from "./reverseString";
 
-test("capitalize basketball", () => {
+test("capitalize: basketball", () => {
   expect(capitalize("basketball")).toStrictEqual("Basketball");
 });
 
-test("capitalize sPOON", () => {
+test("capitalize: sPOON", () => {
   expect(capitalize("sPOON")).toStrictEqual("SPOON");
+});
+
+test("reverse string: shark", () => {
+  expect(reverseString("shark")).toStrictEqual("krahs");
+});
+
+test("reverse string: my name is", () => {
+  expect(reverseString("my name is")).toStrictEqual("si eman ym");
 });
